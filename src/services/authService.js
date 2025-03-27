fb21f0ff8b9e4ccb429e8f31f2dc4bb3f39a7603
muhaiminjobayer <mmj0993@gmail.com>
2025-03-27 18:28:15 -0500

    Add password reset and logout functionality to auth service

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,26 +3,62 @@ import api from './api';
 const authService = {
   // Login user
   login: async (userData) => {
-    const response = await api.post('/auth/signin', userData);
-    return response.data;
+    try {
+      const response = await api.post('/auth/signin', userData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
 
   // Register user
   register: async (userData) => {
-    const response = await api.post('/auth/signup', userData);
-    return response.data;
+    try {
+      const response = await api.post('/auth/signup', userData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
 
   // Get current user details
   getCurrentUser: async () => {
-    const response = await api.get('/auth/user');
-    return response.data;
+    try {
+      const response = await api.get('/auth/user');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
 
   // Logout user
   logout: async () => {
-    const response = await api.post('/auth/signout');
-    return response.data;
+    try {
+      const response = await api.post('/auth/signout');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // Request password reset
+  requestPasswordReset: async (email) => {
+    try {
+      const response = await api.post('/auth/forgot-password', { email });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // Reset password with token
+  resetPassword: async (token, newPassword) => {
+    try {
+      const response = await api.post('/auth/reset-password', { token, newPassword });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
 };
 
