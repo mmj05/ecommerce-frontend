4fa7874d9e252d31889095b6221e9a6b10df4c88
muhaiminjobayer <mmj0993@gmail.com>
2025-03-27 18:28:30 -0500

    Integrate React Router with protected and public routes for user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import { getCurrentUser } from './features/auth/authSlice';
 
 // Pages
 import Home from './pages/Home';
-import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
+// import Products from './pages/Products';
+// import ProductDetails from './pages/ProductDetails';
+// import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Checkout from './pages/Checkout';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+//import Checkout from './pages/Checkout';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
 
 // Components
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import ProtectedRoute from './components/common/ProtectedRoute';
 
 function App() {
   const dispatch = useDispatch();
@@ -27,19 +30,28 @@ function App() {
   }, [dispatch]);
 
   return (
+    
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
+            {/* <Route path="/products" element={<Products />} />
             <Route path="/products/:id" element={<ProductDetails />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<Cart />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
+            
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute />}>
+              {/* <Route path="/checkout" element={<Checkout />} /> */}
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/orders" element={<Profile />} />
+            </Route>
+            
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
