b380954b70655692dbe063934a4d3271e4828f27
muhaiminjobayer <mmj0993@gmail.com>
2025-03-28 04:58:55 -0500

    Add ProductsList component to display a grid of product cards

---
+++ src/components/products/ProductsList.jsx
@@ -0,0 +1,13 @@
+import ProductCard from './ProductCard';
+
+const ProductsList = ({ products }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product.productId} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductsList;
