c4bf0f3f64e961736704c0aaef009cef695d2259
muhaiminjobayer <mmj0993@gmail.com>
2025-04-10 13:14:01 -0500

    Set up Redux store with reducers for auth, cart, products, address, and orders

--- src/store.js
+++ src/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import cartReducer from './features/cart/cartSlice';
 import productReducer from './features/products/productSlice';
+import addressReducer from './features/address/addressSlice';
+import orderReducer from './features/orders/orderSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
     products: productReducer,
+    address: addressReducer,
+    order: orderReducer,
   },
 });
